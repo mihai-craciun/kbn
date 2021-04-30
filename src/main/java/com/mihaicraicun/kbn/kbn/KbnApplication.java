ae8380de00b3f5706ccd0d6dae8e8771f964e4f7
Mihai Craciun <mihaicraciun96@gmail.com>
2021-04-30 18:03:26 +0200

    Initial commit
    - h2 dev in-memory db
    - mock admin and user accounts
    - mock UI
    
    Signed-off-by: Mihai Craciun <mihaicraciun96@gmail.com>

---
+++ src/main/java/com/mihaicraicun/kbn/kbn/KbnApplication.java
@@ -0,0 +1,13 @@
+package com.mihaicraicun.kbn.kbn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mihaicraicun/kbn/kbn/KbnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mihaicraicun.kbn.kbn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
